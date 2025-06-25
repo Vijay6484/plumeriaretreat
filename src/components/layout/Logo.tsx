ec08ca419969e48572181e469ae25b74ab0d76de
chandan-byt <chand76584@gmail.com>
2025-06-25 23:09:50 +0530

    changes

--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import plumeriaLogo from './plumeria-removebg-preview.png'; // Adjust the path as necessary
 interface LogoProps {
   color?: string;
   size?: number;
@@ -8,6 +8,7 @@ interface LogoProps {
 }
 
 const Logo: React.FC<LogoProps> = ({ color = 'currentColor', size = 32, bgColor, scale = 1.5 }) => {
+  // console.log("logo file", plumeriaLogo);
   return (
     <span
       className="inline-flex items-center justify-center rounded"
@@ -21,7 +22,7 @@ const Logo: React.FC<LogoProps> = ({ color = 'currentColor', size = 32, bgColor,
       }}
     >
       <img 
-        src="./../public/plumeria-removebg-preview.png" // Adjust the path as necessary
+        src={plumeriaLogo} // Adjust the path as necessary
         // src="/home/shubham/Project_Paid/Plumeria_2/plumeriaretreat/public/plumeria-removebg-preview.png"
         alt="Plumeria Retreat"
         style={{
